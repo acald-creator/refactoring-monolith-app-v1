af4ccda41161e6af1d2014ea41aaba8583c3246d
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-10 20:20:42 -0600

    Fixed the routing to include the basic get

--- lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
+++ lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
@@ -1,9 +1,40 @@
-const Router = require('@koa/router')
-// Nested the routers together
-const feed = new Router()
-const users = new Router()
-const router = new Router()
+import { FeedItem } from "./feed/models/FeedItem";
+import * as AWS from "../../aws";
 
-feed.user('/feed')
-users.user('/users')
-router.get('/')
+const Router = require("@koa/router");
+// Nested the routers together
+const feed = new Router();
+const users = new Router();
+// const router = new Router()
+const router: typeof Router = Router();
+// Retrieve all items
+router.get("/", async (ctx, next) => {
+    const items = await FeedItem.findAndCountAll({
+        order: [["id", "DESC"]],
+    });
+    items.rows.map((item) => {
+        if (item.url) {
+            item.url = AWS.getGetSignedUrl(item.url); // setup the aws config file @TODO
+        }
+    });
+    next(items);
+});
+// Retrieve a specific source
+router.get("/:id", async (ctx, next) => {
+    let { id } = ctx.params;
+    const item = await FeedItem.findByPk(id);
+    next(item);
+});
+// Update a specific resource
+router.patch('/:id', async (ctx, next) => {
+    //@TODO Finish this section
+    next()
+})
+//@TODO Setup the last section to use auth and signed url
+
+//@TODO
+feed.user("/feed");
+//@TODO
+users.user("/users");
+
+export const IndexRouter: typeof Router = router
