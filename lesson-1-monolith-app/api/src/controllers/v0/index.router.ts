aea554ae2874a67d28727910972fa52e414c4fa6
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-10 20:46:23 -0600

    Fixed the spacing, the double quotes and semis

--- lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
+++ lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
@@ -1,40 +1,40 @@
-import { FeedItem } from "./feed/models/FeedItem";
-import * as AWS from "../../aws";
+import { FeedItem } from './feed/models/FeedItem'
+import * as AWS from '../../aws'
 
-const Router = require("@koa/router");
+const Router = require('@koa/router')
 // Nested the routers together
-const feed = new Router();
-const users = new Router();
+const feed = new Router()
+const users = new Router()
 // const router = new Router()
-const router: typeof Router = Router();
+const router: typeof Router = Router()
 // Retrieve all items
-router.get("/", async (ctx, next) => {
-    const items = await FeedItem.findAndCountAll({
-        order: [["id", "DESC"]],
-    });
-    items.rows.map((item) => {
-        if (item.url) {
-            item.url = AWS.getGetSignedUrl(item.url); // setup the aws config file @TODO
-        }
-    });
-    next(items);
-});
+router.get('/', async (ctx, next) => {
+  const items = await FeedItem.findAndCountAll({
+    order: [['id', 'DESC']]
+  })
+  items.rows.map((item) => {
+    if (item.url) {
+      item.url = AWS.getGetSignedUrl(item.url) // setup the aws config file @TODO
+    }
+  })
+  next(items)
+})
 // Retrieve a specific source
-router.get("/:id", async (ctx, next) => {
-    let { id } = ctx.params;
-    const item = await FeedItem.findByPk(id);
-    next(item);
-});
+router.get('/:id', async (ctx, next) => {
+  const { id } = ctx.params
+  const item = await FeedItem.findByPk(id)
+  next(item)
+})
 // Update a specific resource
 router.patch('/:id', async (ctx, next) => {
-    //@TODO Finish this section
-    next()
+  // @TODO Finish this section
+  next()
 })
-//@TODO Setup the last section to use auth and signed url
+// @TODO Setup the last section to use auth and signed url
 
-//@TODO
-feed.user("/feed");
-//@TODO
-users.user("/users");
+// @TODO
+feed.user('/feed')
+// @TODO
+users.user('/users')
 
 export const IndexRouter: typeof Router = router
