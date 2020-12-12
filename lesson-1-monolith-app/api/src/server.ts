df817e750ec550d5cbaab08c96ca83580d989528
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-12 16:04:35 -0600

    Added line to have sequelize authenticate

--- lesson-1-monolith-app/api/src/server.ts
+++ lesson-1-monolith-app/api/src/server.ts
@@ -1,14 +1,43 @@
-import http from 'http'
+import Hapi from '@hapi/hapi'
+import { sequelize } from './sequelize'
+import { V0MODELS } from './controllers/v0/model.index'
 
-const hostname = 'localhost'
-const port = 8080
+const dotenv = require('dotenv')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
+dotenv.config()
+
+const init = async () => {
+  await sequelize.authenticate().then(() => {
+    console.log('Success!')
+  }).catch((err) => {
+    console.log(err)
+  })
+
+  await sequelize.addModels(V0MODELS)
+  await sequelize.sync()
+
+  const hostname = 'localhost'
+
+  const server = Hapi.server({
+    port: process.env.PORT || 8080,
+    host: hostname
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      h.send('/api/v0')
+    }
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+process.on('uncaughtException', (err) => {
+  console.log(err)
+  process.exit(1)
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+init()
