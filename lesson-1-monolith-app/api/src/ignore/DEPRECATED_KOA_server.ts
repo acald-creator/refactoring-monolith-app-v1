f2809d16711d9a109fb4d2f2c579db71d6b99103
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-12 13:20:57 -0600

    Moved the deprecated files to the ignore folder

---
+++ lesson-1-monolith-app/api/src/ignore/DEPRECATED_KOA_server.ts
@@ -0,0 +1,34 @@
+import { sequelize } from '../sequelize'
+import { IndexRouter } from '../controllers/v0/index.router'
+import { V0MODELS } from '../controllers/v0/model.index'
+
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+// const cors = require('@koa/cors')
+
+(async () => {
+  await sequelize.addModels(V0MODELS)
+  await sequelize.sync()
+
+  const app = new Koa()
+  const port = process.env.PORT || 8080
+
+  //  app.use(cors())
+
+  app.use(bodyParser({
+    detectJSON: function (ctx) {
+      return /\.json$/i.test(ctx.path)
+    }
+  }))
+
+  app.use('/api/v0', IndexRouter)
+
+  app.use('/', async (ctx, next) => {
+    next('/api/v0')
+  })
+
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`)
+    console.log('press CTL+C to stop server')
+  })
+})()
