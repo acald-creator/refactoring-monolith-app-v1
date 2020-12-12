f67857adb9d8b5db534df9d4caa3709b4cae1b91
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-12 13:20:40 -0600

    Testing out nodejs default server file

--- lesson-1-monolith-app/api/src/server.ts
+++ lesson-1-monolith-app/api/src/server.ts
@@ -1,34 +1,14 @@
-import { sequelize } from './sequelize'
-import { IndexRouter } from './controllers/v0/index.router'
-import { V0MODELS } from './controllers/v0/model.index'
+import http from 'http'
 
-const Koa = require('koa')
-const bodyParser = require('koa-bodyparser')
-const cors = require('@koa/cors')
+const hostname = 'localhost'
+const port = 3000
 
-(async () => {
-  await sequelize.addModels(V0MODELS)
-  await sequelize.sync()
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+})
 
-  const app = new Koa()
-  const port = process.env.PORT || 8080
-
-  app.use(cors())
-
-  app.use(bodyParser({
-    detectJSON: function (ctx) {
-      return /\.json$/i.test(ctx.path)
-    }
-  }))
-
-  app.use('/api/v0', IndexRouter)
-
-  app.use('/', async (ctx, next) => {
-    next('/api/v0')
-  })
-
-  app.listen(port, () => {
-    console.log(`server running http://localhost:${port}`)
-    console.log('press CTL+C to stop server')
-  })
-})()
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
