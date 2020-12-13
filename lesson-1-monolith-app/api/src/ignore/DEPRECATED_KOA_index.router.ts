97653bf3548871472a4f7e02dcd29a9aaec59810
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-12 18:29:18 -0600

    This is the deprecated file with using Koa

---
+++ lesson-1-monolith-app/api/src/ignore/DEPRECATED_KOA_index.router.ts
@@ -0,0 +1,37 @@
+import { FeedItem } from "./feed/models/FeedItem";
+import * as AWS from "../../aws";
+
+const Router = require("@koa/router");
+
+const router: typeof Router = Router();
+// Retrieve all items
+router.get("/", async (ctx, next) => {
+  const items = await FeedItem.findAndCountAll({
+    order: [["id", "DESC"]],
+  });
+  items.rows.map((item) => {
+    if (item.url) {
+      item.url = AWS.getGetSignedUrl(item.url); // setup the aws config file @TODO
+    }
+  });
+  next(items);
+});
+// Retrieve a specific source
+router.get("/:id", async (ctx, next) => {
+  const { id } = ctx.params;
+  const item = await FeedItem.findByPk(id);
+  next(item);
+});
+// Update a specific resource
+router.patch("/:id", async (ctx, next) => {
+  // @TODO Finish this section
+  next();
+});
+// @TODO Setup the last section to use auth and signed url
+
+// @TODO
+// feed.user('/feed')
+// @TODO
+// users.user('/users')
+
+export const IndexRouter: typeof Router = router;
