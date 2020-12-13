34d47727163592b4c9b1690b7748a4b02aab661e
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-13 16:53:39 -0600

    Installed aws-sdk

---
+++ lesson-2-solution/api/src/config/aws.ts
@@ -0,0 +1,46 @@
+import { config } from "./config";
+import AWS = require("aws-sdk");
+
+const c = config.dev;
+
+// Configure AWS's credentials to load the profile
+const credentials = new AWS.SharedIniFileCredentials({
+  profile: c.aws_profile,
+});
+
+AWS.config.credentials = credentials;
+
+// Make sure it can recognize the S3 bucket that was created earlier
+export const s3 = new AWS.S3({
+  signatureVersion: "v4",
+  region: c.aws_region,
+  params: {
+    Bucket: c.aws_media_bucket,
+  },
+});
+
+// Be sure that the signed url is setup so that when uploading the images
+// the s3 bucket, it has some security with using signed url
+export function getGetSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  const url = s3.getSignedUrl("getObject", {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return url;
+}
+
+export function getPutSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  const url = s3.getSignedUrl("putObject", {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return url;
+}
---
+++ lesson-2-solution/api/src/config/config.ts
@@ -0,0 +1,25 @@
+export const config = {
+  dev: {
+    username: process.env.POSTGRES_USERNAME || "udagramacald",
+    password: process.env.POSTGRES_PASSWORD || "Password123456",
+    database: process.env.POSTGRES_DB || "udagramdev1",
+    host:
+      process.env.POSTGRES_HOST ||
+      "udagramdev2.ci4aqbvaskx1.us-east-2.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: process.env.AWS_REGION || "us-east-2",
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_BUCKET,
+    aws_secret_name: process.env.AWS_SECRET_NAME,
+  },
+  prod: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    dialect: "postgres",
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET!,
+  },
+};
