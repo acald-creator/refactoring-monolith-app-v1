6a81576e6f8a694430e3f29eb4f09edbd0f88cc3
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-12 20:24:16 -0600

    Added path for the user

--- lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
+++ lesson-1-monolith-app/api/src/controllers/v0/index.router.ts
@@ -1,22 +1,19 @@
 import { FeedRouter } from "./feed/routes/feed.router";
 import { UserRouter } from "./users/routes/user.router";
 
-
 export function IndexRouter() {
   const Call = require("@hapi/call");
 
   const router = new Call.Router();
 
   router.use("/feed", FeedRouter);
-  router.use('/user', UserRouter);
+  router.use("/user", UserRouter);
 
-  router.add(
-    {
-      method: "GET",
-      path: "/",
-      handler: async (request, h) => {
-        return `V0`
-      }
-    }
-  );
+  router.add({
+    method: "GET",
+    path: "/",
+    handler: async (request, h) => {
+      return `V0`;
+    },
+  });
 }
--- lesson-1-monolith-app/api/src/controllers/v0/users/routes/user.router.ts
+++ lesson-1-monolith-app/api/src/controllers/v0/users/routes/user.router.ts
@@ -1,28 +1,27 @@
-import { User } from "../models/Users"
-import { AuthRouter } from "./auth.router"
+import { User } from "../models/Users";
+import { AuthRouter } from "./auth.router";
 
 export function UserRouter() {
-    const Call = require("@hapi/call")
-    const router = new Call.Router()
+  const Call = require("@hapi/call");
+  const router = new Call.Router();
 
-    router.use("/auth", AuthRouter)
+  router.use("/auth", AuthRouter);
 
-    router.add(
-        {
-            method: "GET",
-            path: "/",
-            handler: async (request, h) => {
-            }
-        },
-        {
-            method: "GET",
-            path: "/:id",
-            handler: async (request, h) => {
-                let { id } = request.params
-                const item = await User.findByPk(id)
+  router.add(
+    {
+      method: "GET",
+      path: "/",
+      handler: async (request, h) => {},
+    },
+    {
+      method: "GET",
+      path: "/:id",
+      handler: async (request, h) => {
+        const { id } = request.params;
+        const item = await User.findByPk(id);
 
-                return item
-            }
-        }
-    )
+        return item;
+      },
+    }
+  );
 }
