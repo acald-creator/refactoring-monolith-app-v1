5f525e42a81391f347d3f53bb55668f0a770acac
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-13 20:50:14 -0600

    Installed jimp and setup the util file to help download, filter and save the filtered image locally

---
+++ lesson-0-starter-app/src/util/util.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+import Jimp from 'jimp'
+
+async function filterImageFromURL(inputUrl: string): Promise<string> {
+    return new Promise(async resolve => {
+        const photo = await Jimp.read(inputUrl)
+        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg'
+        await photo.resize().quality(60).grayscale().write(__dirname+outpath, (img)=>{ resolve(__dirname+outpath)
+        })
+    })
+}
+
+async function deleteLocalFiles(files: Array<string>) {
+    for ( let file of files) {
+        fs.unlinkSync(file)
+    }
+}
