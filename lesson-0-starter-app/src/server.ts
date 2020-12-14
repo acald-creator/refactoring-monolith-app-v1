4fc3623d3802a6c054fcfa0c9995113e6cef071f
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-14 08:19:22 -0600

    Successfully setup server.ts

--- lesson-0-starter-app/src/server.ts
+++ lesson-0-starter-app/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { deleteLocalFiles, filterImageFromURL } from "./util/util";
 const app = express()
 
 const port = process.env.PORT || 8082
@@ -8,6 +9,21 @@ app.use(bodyParser.json())
 
 app.get('/', async (req, res) => {
     res.send('Try "GET" /filteredimage?image={{}}')
+})
+
+app.get('/filteredimage', async(request, reply) => {
+    let { imageURL } = request.query
+    if (!imageURL) {
+        return reply.status(400).send(`Invalid url or No url provided`)
+    }
+
+    // @ts-ignore
+    filterImageFromURL(imageURL).then(filteredPath => {
+        reply.status(200).sendFile(filteredPath, () => {
+            // @ts-ignore
+            return deleteLocalFiles(filteredPath)
+        })
+    })
 })
 
 app.listen(port, () => {
